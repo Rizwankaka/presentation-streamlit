44e14a0980c5be8945517e93a9eb0ce4684581d0
Rizwan Rizwan <122643396+Rizwankaka@users.noreply.github.com>
2024-03-01 16:32:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# import streamlit as st
-# import streamlit.components.v1 as components
-# st.title('🎈 Data Storyboard')
-# components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQuRgMB0NvkKx8b2ZLPgiDUBZyQBFLLHAUJQq40fTFoZD15sMo1Vj9dEBxs3kfIlQYOne-PFGZUiIsU/embed?start=false&loop=false&delayms=3000", height=480)
-
 import streamlit as st
 import streamlit.components.v1 as components
 
